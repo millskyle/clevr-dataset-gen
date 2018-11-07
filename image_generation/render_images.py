f4eb22dd9a258d858e5a0da13e165d810d8692b3
Kyle Mills <kyle.mills@uoit.net>
2018-11-07 11:56:38 -0800

    changes

--- image_generation/render_images.py
+++ image_generation/render_images.py
@@ -359,8 +359,8 @@ def add_random_objects(scene_struct, num_objects, args, camera, attempts=0):
   positions = []
   objects = []
   blender_objects = []
-  xrange = [-3,3]
-  yrange = [-3,3]
+  xrange = [-3.0,3.0]
+  yrange = [-3.0,3.0]
   corners = [[xrange[0], yrange[0]],
              [xrange[0], yrange[1]],
              [xrange[1], yrange[0]],
