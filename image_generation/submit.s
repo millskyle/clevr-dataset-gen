7db3acd12391ba621b31e3faa7b468bf3d822328
Kyle Mills <kyle.mills@uoit.net>
2018-11-07 13:02:45 -0800

    updated base scnee tooriginal

--- image_generation/submit.s
+++ image_generation/submit.s
@@ -5,4 +5,4 @@
 
 module load cuda
 
-blender --background -noaudio --python render_images.py -- --height=256 --width=256 --use_gpu=1 --min_objects=2 --max_objects=10 --num_images=10 --margin=0.05
+blender --background -noaudio --python render_images.py -- --height=256 --width=256 --use_gpu=1 --min_objects=2 --max_objects=10 --num_images=10 --margin=0.05 --base_scene_blendfile="./data/water.blend"
