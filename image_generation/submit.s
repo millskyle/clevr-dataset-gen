7d37e219ce805d5359eb5ddf8c69d9debd6abf1b
Kyle Mills <kyle.mills@uoit.net>
2018-11-07 16:17:35 -0800

    submit.s

--- image_generation/submit.s
+++ image_generation/submit.s
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
 #SBATCH -p shockwave
-#SBATCH -t 00:10:00
+#SBATCH -t 01:00:00
 
 module load cuda
-
-blender --background -noaudio --python render_images.py -- --height=256 --width=256 --use_gpu=1  --num_images=10 --base_scene_blendfile="./data/water.blend" --num_ducks=3 --num_balls=3
+blender --background -noaudio --python render_images.py -- --height=256 --width=256 --use_gpu=1  --num_images=10 --base_scene_blendfile="./data/water.blend" --num_ducks=$1 --num_balls=$2
