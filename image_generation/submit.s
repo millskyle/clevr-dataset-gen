0d0d14b13247fb2dee4a7a0aa3b186242dc2979e
Kyle Mills <kyle.mills@uoit.net>
2018-11-14 16:07:43 -0500

    submit_many.sh

--- image_generation/submit.s
+++ image_generation/submit.s
@@ -1,30 +1,32 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
-#SBATCH -p shockwave
 #SBATCH -J ducky
-#SBATCH -t 24:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -t 1:20:00
 
 module load blender
 
 numducks=$1
-scene=$2
+numballs=$2
+run=$3
 
-
-for balls in 0 1 2 3 4 5; do
+for scene in vancouver mountains lake vancouver2; do 
 
 (blender --background -noaudio --python render_images.py -- \
 --height=256 \
 --width=256 \
---use_gpu=1  \
---filename_prefix=${scene}_${numducks}_${balls} \
+--use_gpu=1 \
+--filename_prefix=${run}$RANDOM$RANDOM$RANDOM_${scene}_${numducks}_${numballs} \
 --num_images=1000 \
 --camera_jitter=1 \
 --base_scene_blendfile="./data/base_scenes/${scene}.blend" \
 --num_ducks=$numducks \
---num_balls=$balls
+--num_balls=$numballs
+)  & 
+#) > /dev/null 2>&1 & 
 #--save_blendfiles=1 \
-) &  
 
 done
+
 wait
+
 
---
+++ image_generation/submit_many.sh
@@ -0,0 +1,19 @@
+
+function ducksballs () {
+    ducks=$1
+    balls=$2
+    for run in c; do 
+#        for scene in vancouver mountains vancouver2 lake; do
+            node=$(echo -e "bumblebee\nstarscream\nshockwave\nratchet\narcee" | shuf | head -n 1)
+            sbatch -J ${run}${ducks}ducks -p cpu --nodelist=$node submit.s $ducks $balls $run
+ #       done
+    done
+}
+
+
+
+for pair in $(cat combos); do
+    echo ducksballs $(echo $pair | sed  's|-| |')
+    ducksballs $(echo $pair | sed  's|-| |')
+done
+
