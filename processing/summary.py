4f06a063a9b5ad8670a2729beb1e529e70462589
Kyle Mills <kyle.mills@uoit.net>
2019-03-15 13:50:52 -0400

    added summary.py

--- processing/summary.py
+++ processing/summary.py
@@ -1,9 +1,13 @@
 import glob
+import sys
+import numpy as np
+from shared import scenes
 
+try:
+    task=sys.argv[1]
+except:
+    task='PRINT'
 
-scenes = ["lake", "mountains", "vancouver", "vancouver2"]
-
-scenes=["sunset"]
 
 labels = [i.split('/')[-1] for i in glob.glob("../output/images/*")]
 
@@ -11,10 +15,22 @@ summary = []
 for scene in scenes:
     for label in labels:
         files = glob.glob("../output/images/{}/*{}*.png".format(label,scene))
-        summary.append([scene, label, len(files)])
-
-
+        summary.append([scene, label, len(files), files])
 
 summary = sorted(summary, key=lambda x:x[2])
-for identifier in summary:
-    print("{}\t{}\t{}".format(*identifier))
+np.random.shuffle(summary)
+
+
+if task=='PRINT':
+    for identifier in summary:
+        print("{0:<15s}{1:<15s}\t{2:<15d}".format(*identifier[0:3]))
+
+elif task=='GETWORK':
+    num=1024
+    for identifier in summary:
+        if identifier[-2] < num:
+            remaining=int(np.log2(num-identifier[-2]))
+            ducks,balls = [int(i) for i in identifier[1].split("-")]
+            for _ in range(remaining):
+                print("sbatch -J {scene}{ducks}{balls} -p cpu ../image_generation/submit.s {ducks} {balls} {scene} {random}".format(ducks=ducks, balls=balls, scene=identifier[0], random=np.random.randint(0,9999)))
+
